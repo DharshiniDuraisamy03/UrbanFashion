d516f7dfc95cd0983f3ce3bda0abf1386c73c56d
Dharshu <dharshiniduraisamy2801@gmail.com>
2024-09-06 21:08:28 +0530

    Installed Node

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <body>
+         Hello
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
